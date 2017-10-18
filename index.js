c0f6ad6b82afbb988842ef21a1eb3a2dd6da22ee
Aniruddh Gopale <anigopale@gmail.com>
2017-10-18 14:56:47 +0530

    create index.js, setup server

---
+++ index.js
@@ -0,0 +1,14 @@
+// Main starting point of the application
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+
+// App setup
+
+// Server Setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('server listening on:', port);
