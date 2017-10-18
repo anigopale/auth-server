dae332ec8bb47167670497099c319835b7439cc4
Aniruddh Gopale <anigopale@gmail.com>
2017-10-18 22:29:51 +0530

    add route handler for authentication

--- router.js
+++ router.js
@@ -1,9 +1,13 @@
 const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+const requireAuth = passport.authenticate('jwt', { session: false});
 
 module.exports = function(app) {
-  app.get('/', function(req, res, next) {
-    res.send(['dummy','data']);
-  });
+  app.get('/', requireAuth, function(req, res) {
+    res.send({ hi: 'there'});
+  })
   app.post('/signup', Authentication.signup);
 }
 // app.requesttype('route', function(req, res,next) {
